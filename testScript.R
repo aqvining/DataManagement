8ec6a83e7c86de3468d1af01a2503bf62b82f5ec
Alexander Vining <aqvining@ucdavis.edu>
2016-11-01 13:58:58 -0700

    added linear model simulation

--- testScript.R
+++ testScript.R
@@ -1,2 +1,14 @@
+###this script creates data and a few plots###
+
 x = rnorm(n = 1000, mean = 0, sd = 3)
 hist(x, nclass = 20)
+
+###linear regression
+
+m = 3
+b = -2
+eps = rnorm(1000,0, 10) #1000 residuals drawn from normal distribution around zero with sd of 10
+
+x = runif(1000, 0, 100)
+y = m*x + b + eps
+plot(x,y, cex = .5, col = "blue")
